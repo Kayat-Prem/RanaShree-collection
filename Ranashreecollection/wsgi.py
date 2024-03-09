b9fe3ceba7ccec9a8a77b0dbc8b61d1a9109b48f
Kayat-Prem <imkayat7@gmail.comm>
2024-03-10 00:59:33 +0545

    Changes format in files and Login, Signup and Forget Password  Completed

--- Ranashreecollection/wsgi.py
+++ Ranashreecollection/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Khimacollection project.
+WSGI config for Ranashreecollection project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Khimacollection.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Ranashreecollection.settings")
 
 application = get_wsgi_application()
